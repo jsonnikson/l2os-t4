f140ab3cd24dff6caf73e5718e77fd891575324c
Joshua <joshua@metaphysic.al>
2024-05-17 13:39:09 +0700

    update schema

--- packages/api/src/db/schema.ts
+++ packages/api/src/db/schema.ts
@@ -1,5 +1,5 @@
 import { InferInsertModel, InferSelectModel } from 'drizzle-orm'
-import { integer, real, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { integer, real, sqliteTable, text, unique } from 'drizzle-orm/sqlite-core'
 import { createInsertSchema, createSelectSchema } from 'drizzle-valibot'
 
 // User
@@ -30,3 +30,29 @@ export type Car = InferSelectModel<typeof CarTable>
 export type InsertCar = InferInsertModel<typeof CarTable>
 export const insertCarSchema = createInsertSchema(CarTable)
 export const selectCarSchema = createSelectSchema(CarTable)
+
+// Lexemes
+export const LexemeTable = sqliteTable('Lexeme', {
+  id: text('id').primaryKey(),
+  text: text('text').notNull(),
+})
+
+export type Lexeme = InferSelectModel<typeof LexemeTable>
+export type InsertLexeme = InferInsertModel<typeof LexemeTable>
+export const insertLexemeSchema = createInsertSchema(LexemeTable)
+export const selectLexemeSchema = createSelectSchema(LexemeTable)
+
+// Translations
+export const TranslationTable = sqliteTable('Translation', {
+  id: text('id').primaryKey(),
+  lexemeId: text('lexemeId').notNull().references(() => LexemeTable.id), 
+  text: text('text').notNull(),
+}, t => ({
+  unq: unique().on(t.lexemeId, t.text)
+}))
+
+export type Translation = InferSelectModel<typeof TranslationTable>
+export type InsertTranslation = InferInsertModel<typeof TranslationTable>
+export const insertTranslationSchema = createInsertSchema(TranslationTable)
+export const selectTranslationSchema = createSelectSchema(TranslationTable)
+
